c4555c23fd36853e9c0ca2c01930dc1a581bd75c
Estefani <stef45689@gmail.com>
2021-09-21 10:24:01 -0500

    More Edits

--- choccake-react/src/components/Footer.js
+++ choccake-react/src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render(){
     return (
-      <footer> Delicious &copy; 2013 All Rights Reserved </footer>
+      <footer className = "footer"> Delicious &copy; 2013 All Rights Reserved </footer>
     );
   }
 }
